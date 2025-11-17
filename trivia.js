2e5866174fbb74d4be9671fd7a8c7839b08cea16
Mosakra <liam.edwards@live.ca>
2025-11-17 10:46:03 -0330

    Update trivia.js
    Added a querySelector, Also got all html elements that will need to manipulated need alter the html

---
+++ trivia.js
@@ -0,0 +1,17 @@
+const getElement = selector => document.querySelector(selector);
+
+const startScreen = document.getElement("#start-screen");
+const questionScreen = document.getElement("#question-screen");
+const resultsScreen = document.getElement("#results-screen");
+
+const startBtn = document.getElement("#start-btn");
+const nextBtn = document.getElement("#next-btn");
+const restartBtn = document.getElement("#restart-btn");
+
+const questionText = document.getElement("#question-text");
+const answersContainer = document.getElement("#answers-container");
+const questionCounter = document.getElement("#question-counter");
+const progressFill = document.getElement("#progress-fill");
+
+const finalScore = document.getElement("#final-score");
+const scoreMessage = document.getElement("#score-message");
